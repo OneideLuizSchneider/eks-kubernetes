ec5f42afe3af790d3e27d98faab3fa5305524322
Oneide Luiz Schneider <oneidels@gmail.com>
2021-10-14 00:19:37 -0300

    remove node group

--- terraform/node-group.tf
+++ terraform/node-group.tf
@@ -26,32 +26,3 @@ resource "aws_eks_node_group" "private" {
     Environment = var.env
   }
 }
-
-resource "aws_eks_node_group" "public" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "public-node-group-${var.env}"
-  node_role_arn   = aws_iam_role.node-group.arn
-  subnet_ids      = local.subnets
-
-  labels          = {
-    "type" = "public"
-  }
-
-  instance_types = local.instance_type
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 3
-    min_size     = 1
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.node-group-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.node-group-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.node-group-AmazonEC2ContainerRegistryReadOnly,
-  ]
-
-  tags = {
-    Environment = var.env
-  }
-}
