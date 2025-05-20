b03ebdbceaa4857e29324ead6a02b1a9759c1ee5
OneideLuizSchneider <oneidels@gmail.com>
2025-05-20 19:46:20 +0200

    fix format

--- terraform/local-exec.tf
+++ terraform/local-exec.tf
@@ -1,17 +1,18 @@
 //Updating Kubectl Config File
 resource "null_resource" "update-kube-config" {
-  
+
   provisioner "local-exec" {
     command = <<EOT
-      #!/bin/bash
       aws eks --region ${local.region} update-kubeconfig --name ${local.cluster_name}
 
       kubectl get nodes
       kubectl get pods -A
+
     EOT
   }
 
   depends_on = [
     aws_eks_node_group.private
   ]
+
 }
--- terraform/local.tf
+++ terraform/local.tf
@@ -1,13 +1,13 @@
 locals {
   access_key = "..."
   secret_key = "..."
-  region     = "${var.region}"
+  region     = var.region
 
   vpc_id            = "..."
   subnets           = ["..."]
   internal_ip_range = "0.0.0.0/0"
 
-  eks_version               = "${var.eks_cluster_version}"
+  eks_version               = var.eks_cluster_version
   cluster_name              = "${var.eks_cluster_name}-${var.env}-${var.cluster_version}"
   cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   asg_desired_capacity      = 1
