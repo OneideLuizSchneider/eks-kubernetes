ee280597250bf3d7d7292a1cafc3b0d03e5b2515
Oneide Luiz Schneider <oneidels@gmail.com>
2025-05-20 19:34:28 +0200

    add local.tf and region env

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,20 +18,7 @@ variable "eks_cluster_version" {
   default = "1.31"
 }
 
-locals {
-  access_key = "..."
-  secret_key = "..."
-  region     = "..."
-
-  vpc_id            = "..."
-  subnets           = ["..."]
-  internal_ip_range = "0.0.0.0/0"
-
-  eks_version               = var.eks_cluster_version
-  cluster_name              = "${var.eks_cluster_name}-${var.env}-${var.cluster_version}"
-  cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
-  asg_desired_capacity      = 1
-  asg_max_size              = 2
-  asg_min_size              = 1
-  instance_type             = ["..."]
+variable "region" {
+  type    = string
+  default = "us-east-1"
 }
